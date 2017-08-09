67cdc8b2148cb2d7f1aaaf012a45d8d52ec8405d
Joseph Kim <email": null>
2017-08-09 16:39:25 -0500

    save work - consider React for dom manipulation

--- google-places-feature-testing/application.js
+++ google-places-feature-testing/application.js
@@ -1,34 +1,80 @@
-var map;
-var service;
-var infowindow;
+// This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
-function initialize() {
-  var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+      var map;
+      var infowindow;
 
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 15
-    });
+      function initMap() {
+        var austin = {lat: 30.2672, lng: -97.7431};
 
-  var request = {
-    location: pyrmont,
-    radius: '500',
-    type: ['restaurant']
-  };
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: austin,
+          zoom: 15
+        });
 
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-}
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        service.textSearch({
+          location: austin,
+          query:'barbeque',
+          radius: 2500,
+          type: ['restaurant']
+        }, callback);
+      }
 
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
-    }
+      function callback(results, status,pagination) {
+        textIt(results)
+        console.log(results)
+        // console.log(pagination)
+        // console.log(pagination.nextPage())
+        // $('#stuff').html(textIt(results))
+        // console.log(results)
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+        }
+      }
+
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      }
+function textIt(result) {
+
+  for (var i = 0; i < result.length;i++)  {
+    // console.log(result[i].geometry)
+    // console.log(result[i].html_attributions)
+    // console.log(result[i].icon)
+    // console.log(result[i].id)
+    // console.log(result[i].name)
+    // console.log(result[i].opening_hours)
+    // console.log(result[i].photos)
+    // console.log(result[i].place_id)
+    // console.log(result[i].rating)
+    // console.log(result[i].reference)
+    // console.log(result[i].types)
+
   }
 }
 
 $( document ).ready(function() {
     console.log( "ready!" );
+    initMap()
+
 });
+
+
+// https://developers.google.com/maps/documentation/javascript/reference#PlaceResult
+//https://developers.google.com/places/supported_types
+//https://developers.google.com/maps/documentation/javascript/places#TextSearchRequests
+
