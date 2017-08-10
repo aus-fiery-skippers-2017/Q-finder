9bd5e1bc771d98951400e09c210d8c60f9cccde8
Sinclair Swan <email": null>
2017-08-10 16:17:31 -0500

    m

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,10 +1,15 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def google_oauth2
   def sign_in_with(provider_name)
+  
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    # binding.pry
     sign_in_and_redirect @user, :event => :authentication
     set_flash_message(:notice, :success, :kind => provider_name) if is_navigational_format?
+  else
+    redirect_to new_user_registration_path
   end
+
 
   def facebook
     sign_in_with "Facebook"
