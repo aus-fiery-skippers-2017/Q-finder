f55a8e1b6bbbf3d2cc8fb6177965ccc62fdc5f5e
Erika Boren <email": null>
2017-08-10 17:21:51 -0500

    add font awesome and styled search bar

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,9 +1,19 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # def google_oauth2
+  skip_before_action :verify_authenticity_token
+
   def sign_in_with(provider_name)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user, :event => :authentication
-    set_flash_message(:notice, :success, :kind => provider_name) if is_navigational_format?
+    # if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication
+      set_flash_message(:notice, :success, :kind => provider_name) if is_navigational_format?
+    # else
+    #   if provider_name == 'Google'
+    #     session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
+    #   elsif provider_name == 'Facebook'
+    #     session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra)
+    #   end
+    #  redirect_to new_user_registration_url
+    # end
   end
 
   def facebook
