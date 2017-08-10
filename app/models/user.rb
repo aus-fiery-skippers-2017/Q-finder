5eb4576527eebd7407f41523cf1b9fcd6ac8d6ab
Erika Boren <email": null>
2017-08-10 15:45:06 -0500

    resolve oath issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,20 @@ class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
 
    def self.from_omniauth(auth)
-    if provider == :facebook
+    # binding.pry
+    if auth.provider == 'facebook'
         where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
          user.email = auth.info.email
         end
-    elsif provider == :google_oauth2
-          data = access_token.info
-           user = User.find_by(email: data['email'])
+    elsif auth.provider == 'google_oauth2'
+          user = User.find_by(email: auth.info.email)
           unless user
           user = User.create(
-             email: data['email'],
-             uid: access_token.uid,
-             provider: access_token.provider,
+             email: auth.info.email,
+             uid: auth.uid,
+             provider: auth.provider,
              password: Devise.friendly_token[0,20],
-             picture_url: access_token.extra.raw_info.picture
+             picture_url: auth.extra.raw_info.picture
           )
         end
      end
